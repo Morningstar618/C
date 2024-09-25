bd5a8273158e462fc5912327336221796685a5f7
Morningstar618 <ayush618officer@gmail.com>
2024-09-25 19:40:14 +0530

    strings DSA + rearranged some things

--- LPAcademy/enum.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    enum Company {GOOGLE, MICROSOFT, EBAY};
-    enum Company Company1 = GOOGLE;
-    
-    printf("%c", Company1);
-    return 0;
-}
--- practice/arrays/2d_arrray.c
+++ practice/arrays/2d_arrray.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
 
-    int A[3][4] = {{1,2,3,4}, {2,4,6,8}, {1,3,5,7}};
-    
+    int A[3][4] = {{1, 2, 3, 4}, {2, 4, 6, 8}, {1, 3, 5, 7}};
+
     int *B[3];
     int **C;
 
-    B[0] = (int *)malloc(4*sizeof(int));
-    B[1] = (int *)malloc(4*sizeof(int));
-    B[2] = (int *)malloc(4*sizeof(int));
+    B[0] = (int *)malloc(4 * sizeof(int));
+    B[1] = (int *)malloc(4 * sizeof(int));
+    B[2] = (int *)malloc(4 * sizeof(int));
 
-    C = (int **) malloc(3*sizeof(int *));
-    C[0] = (int *) malloc(4*sizeof(int));
-    C[1] = (int *) malloc(4*sizeof(int));
-    C[2] = (int *) malloc(4*sizeof(int));
+    C = (int **)malloc(3 * sizeof(int *));
+    C[0] = (int *)malloc(4 * sizeof(int));
+    C[1] = (int *)malloc(4 * sizeof(int));
+    C[2] = (int *)malloc(4 * sizeof(int));
 
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<4; j++)
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
             printf("%d ", A[i][j]);
         printf("\n");
     }
